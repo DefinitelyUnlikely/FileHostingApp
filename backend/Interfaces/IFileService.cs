b465d449124ed78a179fbc2888b897e363283b35
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-24 10:32:52 +0200

    Remove comments about needing DTOs

--- backend/Interfaces/IFileService.cs
+++ backend/Interfaces/IFileService.cs
@@ -1,10 +1,5 @@
 namespace Backend.Interfaces;
 
-// For create and update, we'll send a file DTO. 
-// As I haven't made a class for it yet, Im just leaving this comment.
-// And the Get and GetAll will probably get File models and 
-// remake them into DTOs for the controller, so the return is currently incorrect. 
-// as for the DTO: Remember to make most fields nullable.
 public interface IFileService
 {
     public Task<bool> CreateAsync(DTO.FileDTO fileDTO);
