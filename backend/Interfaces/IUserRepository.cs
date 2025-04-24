a774fca07b019291e112d5f5f154517e88a23fc4
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-24 11:51:26 +0200

    Add comment about the viablity of having a userrepo at all when using IdentityCore

--- backend/Interfaces/IUserRepository.cs
+++ backend/Interfaces/IUserRepository.cs
@@ -1,5 +1,7 @@
 namespace Backend.Interfaces;
 
+// Is this at all needed? the IdentityCore UserManager will do most
+// of the things I want this to do, won't it?
 public interface IUserRepository
 {
 
