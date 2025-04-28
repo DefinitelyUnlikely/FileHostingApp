5dadd00caa289b4526d5be051531f18f9471d26a
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-28 13:04:08 +0200

    Fix searchservice interface

--- backend/Interfaces/ISearchServices.cs
+++ backend/Interfaces/ISearchServices.cs
@@ -2,7 +2,7 @@ namespace Backend.Interfaces;
 
 public interface ISearchService
 {
-    public Task<ICollection<FileInfo>> FindFilesByTextAsync(string text);
-    public Task<ICollection<FileInfo>> FindFilesByDateAsync(DateTime fromDate, DateTime toDate, bool byCreatedDate = true);
+    public Task<ICollection<Models.FileInfo>> FindFilesByTextAsync(string text);
+    public Task<ICollection<Models.FileInfo>> FindFilesByDateAsync(DateTime fromDate, DateTime toDate, bool byCreatedDate = true);
     public Task<ICollection<Models.Folder>> FindFolderAsync(string name);
 }
--- backend/Services/FileService.cs
+++ backend/Services/FileService.cs
@@ -15,7 +15,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 
@@ -27,7 +27,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 
@@ -39,7 +39,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 
@@ -51,7 +51,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 
@@ -63,7 +63,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 
@@ -75,7 +75,7 @@ public class FileService(IFileRepository fileRepository) : IFileService
         }
         catch (Exception e)
         {
-            throw new RepositoryException("Repository error: " + e.Message + "StackTrace" + e.StackTrace);
+            throw new RepositoryException(e.Message + "StackTrace" + e.StackTrace);
         }
     }
 }
