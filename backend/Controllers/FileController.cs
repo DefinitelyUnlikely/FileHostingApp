790251a7c2d97fd2375449238fae919a3ba5d063
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-05 13:06:28 +0200

    Add the controller rotues I believe I need atm

--- backend/Controllers/FileController.cs
+++ backend/Controllers/FileController.cs
@@ -1,1 +1,35 @@
+using Backend.Interfaces;
+using Backend.DTO;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.Controllers;
+
+[ApiController]
+[Route("file")]
+public class FileController(IFileService fileService) : ControllerBase
+{
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadFile([FromBody] CreateFileRequest request)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{fileId}")]
+    public async Task<IActionResult> GetFile(string fileId)
+    {
+        return Ok();
+    }
+
+    [HttpPatch("update/{fileId}")]
+    public async Task<IActionResult> UpdateFile([FromBody] UpdateFileRequest request, string fileId)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{fileId}")]
+    public async Task<IActionResult> DeleteFile(string fileId)
+    {
+        return Ok();
+    }
+}
--- backend/Controllers/FolderController.cs
+++ backend/Controllers/FolderController.cs
@@ -1,1 +1,35 @@
+using Backend.DTO;
+using Backend.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.Controllers;
+
+[ApiController]
+[Route("folder")]
+public class FolderController(IFolderService folderService) : ControllerBase
+{
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> CreateFolder([FromBody] CreateFolderRequest request)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{folderId}")]
+    public async Task<IActionResult> GetFolder(string folderId)
+    {
+        return Ok();
+    }
+
+    [HttpPatch("update/{folderId}")]
+    public async Task<IActionResult> UpdateFolder([FromBody] UpdateFolderRequest request, string folderId)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{folderId}")]
+    public async Task<IActionResult> DeleteFolder(string folderId)
+    {
+        return Ok();
+    }
+}
