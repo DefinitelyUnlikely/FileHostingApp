1f0470d701538a91cbb6daacb1f58d1084b62b8c
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-07 12:53:07 +0200

    Add try catch to folder service, add catch logic

--- backend/Controllers/FolderController.cs
+++ backend/Controllers/FolderController.cs
@@ -1,4 +1,5 @@
 using Backend.DTO;
+using Backend.Exceptions;
 using Backend.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,24 +13,76 @@ public class FolderController(IFolderService folderService) : ControllerBase
     [HttpPost()]
     public async Task<IActionResult> CreateFolder([FromBody] CreateFolderRequest request)
     {
-        return Ok();
+        try
+        {
+            return Ok();
+        }
+        catch (NoChangesSavedException)
+        {
+            return StatusCode(500);
+        }
+        catch (Exception)
+        {
+            return BadRequest("An unexpected error happened, double check your request data");
+        }
     }
 
     [HttpGet("{folderId}")]
     public async Task<IActionResult> GetFolder(string folderId)
     {
-        return Ok();
+        try
+        {
+            return Ok();
+        }
+        catch (EmptyReturnException)
+        {
+            return NotFound("No folder with that Id was found");
+        }
+        catch (Exception)
+        {
+            return BadRequest("An unexpected error happened, double check your request data");
+        }
     }
 
     [HttpPatch("{folderId}")]
     public async Task<IActionResult> UpdateFolder([FromBody] UpdateFolderRequest request, string folderId)
     {
-        return Ok();
+        try
+        {
+            return Ok();
+        }
+        catch (EmptyReturnException)
+        {
+            return NotFound("No folder with that Id was found");
+        }
+        catch (NoChangesSavedException)
+        {
+            return StatusCode(500);
+        }
+        catch (Exception)
+        {
+            return BadRequest("An unexpected error happened, double check your request data");
+        }
     }
 
     [HttpDelete("{folderId}")]
     public async Task<IActionResult> DeleteFolder(string folderId)
     {
-        return Ok();
+        try
+        {
+            return Ok();
+        }
+        catch (EmptyReturnException)
+        {
+            return NotFound("No folder with that Id was found");
+        }
+        catch (NoChangesSavedException)
+        {
+            return StatusCode(500);
+        }
+        catch (Exception)
+        {
+            return BadRequest("An unexpected error happened, double check your request data");
+        }
     }
 }
