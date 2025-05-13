5161b39d8f1591aded78030e2ab1240d080d7aba
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-13 15:22:55 +0200

    add layout

---
+++ fileapp/src/components/navbar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/register">Register</a>
+	<a href="/login">Login</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: black;
+		width: 100vw;
+		height: 2rem;
+	}
+	a {
+		text-decoration: none;
+		color: white;
+	}
+</style>
---
+++ fileapp/src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Navbar from '../components/navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<Navbar />
+
+{@render children()}
+
+<style>
+	:global(body) {
+		background-color: rgb(41, 41, 41);
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	:global(h1) {
+		color: white;
+	}
+
+	:global(h2) {
+		color: white;
+	}
+
+	:global(p) {
+		color: white;
+	}
+
+	:global(label) {
+		color: white;
+	}
+</style>
