57281e1b73921ba5e24f251f13f8e294e524c19f
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-20 22:41:28 +0200

    add some text about registeration

--- fileapp/src/routes/login/+page.svelte
+++ fileapp/src/routes/login/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { isLoggedIn, user } from '../../stores/auth';
+
 	let email: string = $state('');
 	let password: string = $state('');
 </script>
 
+{#if $isLoggedIn}
+	<p>You are logged in as: {$user}</p>
+{:else}
+	<h4>Login</h4>
+	<p>Don't have an account yet?</p>
+	<p>Register <a href="/register">here</a></p>
+{/if}
+
 <style>
+	p {
+		margin: 0;
+	}
 </style>
