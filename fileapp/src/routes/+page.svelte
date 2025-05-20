f033c1a424263779ba48b646453ef901f0cfb1d1
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-20 23:12:58 +0200

    Add txt file with a color schema for the website to make things look, hopefully, a bit better and more uniform

--- fileapp/src/routes/+page.svelte
+++ fileapp/src/routes/+page.svelte
@@ -5,8 +5,24 @@
 {#if $isLoggedIn}
 	<button onclick={() => isLoggedIn.set(false)}>Logout</button>
 {:else}
-	<button onclick={() => isLoggedIn.set(true)}>Login</button>
+	<div class="login-window">
+		<label for="email-input">Email:</label>
+		<input type="email" name="email" id="email" />
+		<label for="password" style="margin-top: 1rem;">Password:</label>
+		<input type="password" name="password" id="password" />
+	</div>
 {/if}
 
 <style>
+	.login-window {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-top: 20%;
+		width: 90%;
+		height: 50%;
+		background-color: whitesmoke;
+		border-radius: 10px;
+	}
 </style>
