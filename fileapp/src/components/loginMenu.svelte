b54124ec9086699cb69b79d99fc22bfea9e139db
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-21 13:03:30 +0200

    add some styling to button and input, add comment with style to global.css for copy paste or use with global class

--- fileapp/src/components/loginMenu.svelte
+++ fileapp/src/components/loginMenu.svelte
@@ -39,4 +39,55 @@
 	p {
 		margin: 0;
 	}
+
+	button {
+		--color: #0e79b2ff;
+		font-family: inherit;
+		display: inline-block;
+		width: 6em;
+		height: 2.6em;
+		line-height: 2.5em;
+		overflow: hidden;
+		cursor: pointer;
+		margin: 20px;
+		font-size: 17px;
+		z-index: 1;
+		color: var(--color);
+		border: 2px solid var(--color);
+		border-radius: 6px;
+		position: relative;
+	}
+
+	button::before {
+		position: absolute;
+		content: '';
+		background: var(--color);
+		width: 150px;
+		height: 200px;
+		z-index: -1;
+		border-radius: 50%;
+	}
+
+	button:hover {
+		color: white;
+	}
+
+	button:before {
+		top: 100%;
+		left: 100%;
+		transition: 0.3s all;
+	}
+
+	button:hover::before {
+		top: -30px;
+		left: -30px;
+	}
+
+	input {
+		font-family: inherit;
+		height: 2.6em;
+		line-height: 2.5em;
+		font-size: 17px;
+		border-radius: 6px;
+	}
 </style>
