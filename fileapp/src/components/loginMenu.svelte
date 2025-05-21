ba7d79b54201cf8e3da6566a6438a33167c1adc2
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-21 11:08:33 +0200

    Make loginMenu component for reusablility

---
+++ fileapp/src/components/loginMenu.svelte
@@ -0,0 +1,21 @@
+<div class="login-window">
+	<label for="email-input">Email:</label>
+	<input type="email" name="email" id="email" />
+	<label for="password" style="margin-top: 1rem;">Password:</label>
+	<input type="password" name="password" id="password" />
+</div>
+
+<style>
+	.login-window {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-top: 20%;
+		width: var(--width);
+		height: var(--height);
+		background-color: #ebebebff;
+		border: 2px solid #d6d6d6ff;
+		border-radius: 10px;
+	}
+</style>
--- fileapp/src/routes/+page.svelte
+++ fileapp/src/routes/+page.svelte
@@ -1,29 +1,14 @@
 <script lang="ts">
+	import LoginMenu from '../components/loginMenu.svelte';
+
 	import { isLoggedIn, user } from '../stores/auth';
 </script>
 
 {#if $isLoggedIn}
 	<button onclick={() => isLoggedIn.set(false)}>Logout</button>
 {:else}
-	<div class="login-window">
-		<label for="email-input">Email:</label>
-		<input type="email" name="email" id="email" />
-		<label for="password" style="margin-top: 1rem;">Password:</label>
-		<input type="password" name="password" id="password" />
-	</div>
+	<LoginMenu --height="50%" --width="80%" />
 {/if}
 
 <style>
-	.login-window {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		margin-top: 20%;
-		width: 90%;
-		height: 50%;
-		background-color: #ebebebff;
-		border: 2px solid #d6d6d6ff;
-		border-radius: 10px;
-	}
 </style>
