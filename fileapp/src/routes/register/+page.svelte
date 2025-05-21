49edfb7487e6ef033f072e140c1d6046a253e074
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-21 13:40:24 +0200

    Add some checks for reg info

--- fileapp/src/routes/register/+page.svelte
+++ fileapp/src/routes/register/+page.svelte
@@ -3,27 +3,53 @@
 
 	let email: string = $state('');
 	let password: string = $state('');
+	let repeatPassword: string = $state('');
+
+	let error: string = $state('');
 
 	async function CreateUser() {
-		let body = {
-			email: email,
-			password: password
-		};
+		if (email === '' || password === '' || repeatPassword === '') {
+			error = 'Please enter all required information!';
+		}
 
-		const response = await fetch(callUrl, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(body)
-		});
+		if (password != repeatPassword) {
+			console.log('Passwords did not match!');
+			error = 'Passwords did not match!';
+		}
 
-		console.log(response);
+		// let body = {
+		// 	email: email,
+		// 	password: password
+		// };
+
+		// const response = await fetch(callUrl, {
+		// 	method: 'POST',
+		// 	headers: { 'Content-Type': 'application/json' },
+		// 	body: JSON.stringify(body)
+		// });
+
+		// console.log(response);
 	}
 </script>
 
-<h2>Register user</h2>
+<h2>Register New User</h2>
+
 <label for="username">User email:</label>
-<input type="text" name="email" id="email" bind:value={email} />
+<input class={['custom-input']} type="text" name="email" id="email" bind:value={email} />
+
 <label for="password">Password:</label>
 <input type="password" name="password" id="password" bind:value={password} />
 
-<button onclick={CreateUser}>Create new user</button>
+<label for="repeat-password">Repeat Password:</label>
+<input type="password" name="repeat-password" id="repeat-password" bind:value={repeatPassword} />
+
+<button class="custom-button" onclick={CreateUser}>Create new user</button>
+
+{#if error === ''}
+	<p></p>
+{:else}
+	<p>{error}</p>
+{/if}
+
+<style>
+</style>
