24cea6a89f648dcdf8566f942743ddefb84ba5b4
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-21 12:09:28 +0200

    add more text to homepage when not logged in

--- fileapp/src/routes/+page.svelte
+++ fileapp/src/routes/+page.svelte
@@ -7,8 +7,13 @@
 {#if $isLoggedIn}
 	<button onclick={() => isLoggedIn.set(false)}>Logout</button>
 {:else}
+	<p>Welcome</p>
+	<p>Please login to gain access to your cloudstorage</p>
 	<LoginMenu --height="50%" --width="80%" --top-margin="10%" />
 {/if}
 
 <style>
+	p {
+		text-align: center;
+	}
 </style>
