ec6c46b845e9f0aadd3e25e8fd4c5a171a1feb03
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-23 11:48:06 +0200

    add files to folder model + add a name as of now to metadata, for some testing of concepts

--- fileapp/src/lib/models.ts
+++ fileapp/src/lib/models.ts
@@ -11,16 +11,23 @@ export class Folder {
     public name: string
     public subFolders: Folder[]
     public userid: string // Might be superfluous
+    public files: FileMetadata[]
 
-    public constructor(id: string, name: string, subFolders: Folder[], userId: string) {
+    public constructor(id: string, name: string, subFolders: Folder[], files: FileMetadata[], userId: string) {
         this.id = id;
         this.name = name;
         this.subFolders = subFolders;
+        this.files = files
         this.userid = userId;
     }
 }
 
 export class FileMetadata {
+    public name: string;
+
+    public constructor(name: string) {
+        this.name = name;
+    }
 
 }
 
