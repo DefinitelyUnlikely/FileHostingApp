9d2a0fe470d99e98f1731a85aeb24190470f72a3
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-28 22:49:42 +0200

    Add missing space between bearer and token

--- fileapp/src/routes/+page.server.ts
+++ fileapp/src/routes/+page.server.ts
@@ -5,13 +5,13 @@ import { Logout } from '../stores/auth';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
     if (!cookies.get("userinfo")) {
-        return {}
+        return;
     }
 
     let response = await fetch(API_BASE_URL + "/folder/root", {
         method: 'GET',
         headers: {
-            authorization: "Bearer" + cookies.get("token"),
+            authorization: "Bearer " + cookies.get("token"),
             'X-Include-Files': "true",
             'content-type': 'application/json'
         },
@@ -19,11 +19,10 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
     });
 
     if (!response.ok) {
-        console.log(response.status);
         let refresh = cookies.get("refresh");
         let refreshRespone = await fetch(API_BASE_URL + "/refresh", {
             method: 'POST',
-            body: '{"refreshtoken":' + refresh + '"}'
+            body: '{"refreshtoken": ' + refresh + '"}'
         });
 
         if (!response.ok) {
@@ -33,7 +32,24 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
             cookies.delete("refresh", { path: "/" });
             redirect(303, "/")
         }
-        error(404, "No root folder found")
+
+        let refreshJson = await refreshRespone.json();
+        cookies.set("token", refreshJson.accessToken, { path: "/" })
+        cookies.set("refresh", refreshJson.refreshToken, { path: "/" })
+
+        response = await fetch(API_BASE_URL + "/folder/root", {
+            method: 'GET',
+            headers: {
+                authorization: "Bearer " + cookies.get("token"),
+                'X-Include-Files': "true",
+                'content-type': 'application/json'
+            },
+            body: undefined
+        });
+
+        if (!response.ok) {
+            error(500, "Cannot fetch valid response, try login in again")
+        }
     }
 
     return {
