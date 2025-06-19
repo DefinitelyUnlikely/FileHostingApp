4cf8a63101bf802c1fdd51fe0c64abe62636f629
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-06-19 17:57:29 +0200

    changed the serverside back to not using Refresh function, as it rusn before the browser has loaded

--- fileapp/src/routes/+page.server.ts
+++ fileapp/src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import { API_BASE_URL } from '$lib/config';
 import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { RefreshToken } from '../stores/auth';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
     if (!cookies.get("userinfo")) {
@@ -19,9 +18,25 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
     });
 
     if (!response.ok) {
-        if (!RefreshToken()) {
-            error(401, "Your session has expired. Please log back in.")
+        let refresh = cookies.get("refresh");
+        let refreshRespone = await fetch(API_BASE_URL + "/refresh", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ refreshtoken: refresh })
+        });
+
+        if (!refreshRespone.ok) {
+            cookies.delete("userinfo", { path: "/" });
+            cookies.delete("token", { path: "/" });
+            cookies.delete("refresh", { path: "/" });
+            redirect(303, "/login")
         }
+
+        let refreshJson = await refreshRespone.json();
+        cookies.set("token", refreshJson.accessToken, { path: "/" });
+        cookies.set("refresh", refreshJson.refreshToken, { path: "/" });
 
         response = await fetch(API_BASE_URL + "/folder/root", {
             method: 'GET',
